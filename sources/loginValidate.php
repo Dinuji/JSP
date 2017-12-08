784e686a58b7137969d09298231669330b1c0e50
Dinuji <dinujirandika@gmail.com>
2017-12-08 15:39:55 +0530

    Changed DB

--- sources/loginValidate.php
+++ sources/loginValidate.php
@@ -1,8 +1,8 @@
 <?php
 	
-	$dbhost = "127.0.0.1";
-	$dbuser = "root";
-	$dbpass = "";
+	$dbhost = "uoc-mydb-instance.ciaqpoqp6i0b.us-east-2.rds.amazonaws.com:3306";
+	$dbuser = "jsproot";
+	$dbpass = "jsprootpass";
 	$errMsg = "";
 	
 	$conn = mysql_connect($dbhost,$dbuser,$dbpass);
